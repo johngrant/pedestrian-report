905cee32dcf1471143b09ed492f471d227ec2405
John Grant <2171719+JohnGrant@users.noreply.github.com>
2020-06-11 13:12:12 -0400

    Update readme.

--- src/components/reasons.js
+++ src/components/reasons.js
@@ -3,5 +3,6 @@ const reasons = [
   { name: "Crosswalk button broke", value: "1" },
   { name: "Sidewalk blocked", value: "2" },
   { name: "No sidewalk", value: "3" },
+  { name: "Dangerous Intersection", value: "4" },
 ];
 export default reasons;
